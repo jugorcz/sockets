ee20658dc69b68d01992694b2df4ba690b1ecd20
Justyna Gorczyca <just.gorcz@gmail.com>
2019-03-09 16:56:11 +0100

    zad 3

---
+++ zad3/JavaTcpUdp/src/JavaUdpServer.java
@@ -0,0 +1,43 @@
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.util.Arrays;
+
+public class JavaUdpServer {
+
+    public static void main(String args[])
+    {
+        System.out.println("JAVA UDP SERVER");
+        DatagramSocket socket = null;
+        int portNumber = 9008;
+
+        try{
+            socket = new DatagramSocket(portNumber);
+            byte[] receiveBuffer = new byte[1024];
+
+            Arrays.fill(receiveBuffer, (byte)0);
+            DatagramPacket receivePacket = new DatagramPacket(receiveBuffer, receiveBuffer.length);
+            socket.receive(receivePacket);
+            int number = ByteBuffer.wrap(receiveBuffer).order(ByteOrder.LITTLE_ENDIAN).getInt();
+            System.out.println("received msg: " + number);
+
+            number++;
+            InetAddress address = receivePacket.getAddress();
+            portNumber = receivePacket.getPort();
+            receiveBuffer = ByteBuffer.allocate(4).putInt(number).array();
+            receivePacket = new DatagramPacket(receiveBuffer, receiveBuffer.length, address, portNumber);
+            socket.send(receivePacket);
+
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
+        finally {
+            if (socket != null) {
+                socket.close();
+            }
+        }
+    }
+}
